2645d7271f3d693c960f451229027f73507bd20c
Shaul <skhayyat2@student.toouro,edu>
2025-11-09 13:15:29 -0500

    Perpetual Motion

---
+++ app/build.gradle.kts
@@ -0,0 +1,49 @@
+import org.gradle.kotlin.dsl.implementation
+
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.perpetual_motion"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.perpetual_motion"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(files("pm_game.aar"))
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
+    implementation("com.google.code.gson:gson:2.13.2")
+    implementation(libs.appcompat)
+    implementation(libs.activity)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
